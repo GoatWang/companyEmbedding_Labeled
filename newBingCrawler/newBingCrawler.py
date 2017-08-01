ef1a6f06d757ee31fdb6f3c451a287891e17ce26
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-08-01 21:15:31 +0800

    first

---
+++ newBingCrawler/newBingCrawler.py
@@ -0,0 +1,132 @@
+import aiohttp
+import asyncio
+import async_timeout
+import os
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import queue
+import threading
+from os import listdir
+import pandas as pd
+import string
+from preprocessing import preprocessing
+import json
+
+## Build Queue
+input_companies = queue.Queue()
+fail_log = queue.Queue()
+
+files = listdir("labelData")
+files.pop(0)
+df_comps = pd.read_csv("labelData/" + files[0], index_col=None, header=None)
+
+companies = list(df_comps[0])
+for company in companies:   
+    input_companies.put(company+" product")
+
+
+
+class newBingCrawler:
+    def __init__(self, loop):
+        self.loop = loop  ##self.loop
+
+    def __call__(self):
+        async def fetch_coroutine(client, url):
+            with async_timeout.timeout(10):
+                try: 
+                    async with client.get(url) as response:
+                        assert response.status == 200
+                        contentType = str(response.content_type)
+                        if 'html' in str(contentType).lower():
+                            html = await response.text()
+                            soup = BeautifulSoup(html ,'lxml')
+                            [x.extract() for x in soup.findAll('script')]
+                            [x.extract() for x in soup.findAll('style')]
+                            [x.extract() for x in soup.findAll('nav')]
+                            [x.extract() for x in soup.findAll('footer')]
+                            self.companyInfo += soup.text
+                        return await response.release()
+                except:
+                    self.failLinks.append(url)
+
+        async def main(loop):
+            #driver = webdriver.Chrome()
+            #driver = webdriver.PhantomJS()
+            driver = webdriver.PhantomJS(executable_path="D:\\phantomjs.exe")
+            url = "https://www.bing.com/"
+            driver.get(url)
+            elem = driver.find_element_by_xpath('//*[@id="sb_form_q"]')
+            elem.send_keys(self.query)
+            elem = driver.find_element_by_xpath('//*[@id="sb_form_go"]')
+            elem.submit()
+            html = driver.page_source
+            driver.close()
+
+            soup = BeautifulSoup(html, 'lxml')
+            Links = soup.find_all('a')
+
+            Goodlinks = []
+            for link in Links:
+                linkstr = str(link)
+                if (('http' in linkstr) and ('href' in linkstr) and (not 'href="#"' in linkstr) and (not 'href="http://go.microsoft' in linkstr)and (not 'microsofttranslator' in linkstr)):
+                    Goodlinks.append(link)
+
+            urls = [link['href'] for link in Goodlinks]
+            #print(self.query, "Good links have been found!")
+
+            headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+            async with aiohttp.ClientSession(loop=loop, headers=headers, conn_timeout=5 ) as client:
+                tasks = [fetch_coroutine(client, url) for url in urls]
+                await asyncio.gather(*tasks)
+ 
+        while True:
+            try:
+                self.query = input_companies.get(timeout=1)   ##Build self.query
+            except:
+                break
+            
+            ## build self attr
+            self.companyInfo = ""  ##Build self.companyInfo
+            exclude = set(string.punctuation)
+            companyName = self.query.replace(" product", "")
+            companyName = ''.join(p for p in companyName if p not in exclude)
+            self.companyName = companyName.replace(" ", "_").lower()  ##Build self.companyName
+            self.failLinks = []  ##Build self.failLinks
+
+            ## start running loop
+            self.loop.run_until_complete(main(self.loop))
+
+            ## After loop
+            fail_log.put({self.companyName:self.failLinks})
+            
+            file = open("comapnyEmbedding/" + self.companyName, 'w', encoding='utf8')
+            file.write(self.companyInfo)
+            file.close()
+            file = open("processedCompanyEmbedding/" + self.companyName, 'w', encoding='utf8')
+            file.write(preprocessing(self.companyInfo))
+            file.close()
+
+            print(id(self), companyName + " success")
+
+
+threads = []
+for i in range(4):
+    loop = asyncio.new_event_loop()
+    newthread = threading.Thread(target=newBingCrawler(loop))
+    newthread.start()
+    threads.append(newthread)
+
+for thread in threads:
+    thread.join()
+
+logs = []
+while True:
+    try:
+        logs.append(fail_log.get(timeout=1))
+    except:
+        break
+file = open("log.txt",'w', encoding='utf8')
+file.write(json.dump(logs))
+file.close()
+
