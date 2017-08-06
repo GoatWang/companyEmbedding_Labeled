4f40b96598096fad28e3bf0ad1a6867b3d46862b
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-08-06 21:57:40 +0800

    change to keep data only repo

---
+++ indri/run.sh
@@ -0,0 +1,8 @@
+﻿IndriBuildIndex indexBuilder.param
+dumpindex index/company stats > stats
+
+
+
+IndriBuildIndex ProcessedIndexBuilder.param
+dumpindex index/processedCompany stats > processedstats
+
