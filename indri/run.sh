fd41cc71ff591dadd957cb91d74dea6d656002af
jeremy4555 <jeremy4555@yahoo.com.tw>
2017-08-06 22:05:47 +0800

    add shell scripts

--- indri/run.sh
+++ indri/run.sh
@@ -1,8 +1,6 @@
 ﻿IndriBuildIndex indexBuilder.param
 dumpindex index/company stats > stats
 
-
-
 IndriBuildIndex ProcessedIndexBuilder.param
 dumpindex index/processedCompany stats > processedstats
 
